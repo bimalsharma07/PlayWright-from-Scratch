a2d302dec1baacece09298a3e3ea9cb2a3bf90b2
Bimal Sharma <31bimal@gmail.com>
2025-07-29 01:03:00 +0100

    quick fix

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -5,12 +5,16 @@ export class LoginPage {
     readonly usernameInput: Locator;
     readonly passwordInput: Locator;
     readonly loginButton: Locator;
+    readonly errorMessage: Locator;
+    readonly Message: string = 'Epic sadface: Username and password do not match any user in this service';
     
     constructor(page: Page) {
         this.page = page;
         this.usernameInput = page.getByPlaceholder('Username');
         this.passwordInput = page.getByPlaceholder('Password');
         this.loginButton = page.getByRole('button', {name : 'Login'})
+        this.errorMessage = page.getByText(this.Message);
+
     }
     async goto() {
         const baseURL = process.env.BASE_URL;
@@ -19,7 +23,15 @@ export class LoginPage {
         }
         await this.page.goto(baseURL);
     }
-    async login(USERNAME: string, PASSWORD: string) {
+    async invalidLogin(Invalid_Username: string, Invadlid_Password: string) {
+        await this.usernameInput.fill(Invalid_Username);
+        await this.passwordInput.fill(Invadlid_Password);
+        await this.loginButton.click();
+        await expect(this.errorMessage).toHaveText('Epic sadface: Username and password do not match any user in this service');
+
+    }
+
+    async validlogin(USERNAME: string, PASSWORD: string) {
         await this.usernameInput.fill(USERNAME);
         await this.passwordInput.fill(PASSWORD);
         await this.loginButton.click();
